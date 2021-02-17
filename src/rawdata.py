90acd37f1d9aeed8f03f5011de09489d05eda26b
leo <mumuli52@gmail.com>
2021-02-18 04:27:23 +0800

    增加数据处理脚本

--- src/rawdata.py
+++ src/rawdata.py
@@ -1,21 +1,41 @@
 #!/usr/bin/env python3
 
 import ROOT
+from ROOT import TFile, TTree, gROOT, addressof
 import numpy as np
 
-dataname = '../data/2021.raw'
+rawDataName = '../data/2021.raw'
+rawRootName = 'raw.root'
 
-for line in open(dataname):
+rootFile = ROOT.TFile(rawRootName, "RECREATE")
+tree = ROOT.TTree("raw", "raw data")
+vect = ROOT.std.vector("float")(100)
+tree.Branch("raw", vect)
+maxValue = np.empty((1), dtype=float)
+maxIndex = np.empty((1), dtype=int)
+maxIndexPercent = np.empty((1), dtype=float)
+tree.Branch("maxValue", maxValue, "maxValue/F")
+tree.Branch("maxIndex", maxIndex, "maxIndex/I")
+tree.Branch("maxIndexPercent", maxIndexPercent, "maxIndexPercent/F")
+for line in open(rawDataName):
     nums = line.split(' ')
+    #为了偷懒使用python中的内置函数
     li = list()
     for num in nums:
         if not num: continue
         li.append(float(num.strip()))
     li.reverse()
+    #原始值保存
+    vect.clear()
+    for x in li:
+        vect.emplace_back(x)
+
     total = sum(li)
-    num = len(li)
-    li = [i/total for i in li]
-    maxValue = max(li)
-    maxIndex = li.index(maxValue)
-    mean = np.mean(li)
-    print("total value:%.02f, count:%d, max value:%.02f, index:%d, mean:%.02f" % (total, num, 100 * maxValue, maxIndex, 100 * mean) )
+    count = len(li)
+    li = [100 * i/total for i in li]
+    maxValue[0] = max(li)
+    maxIndex[0] = li.index(maxValue[0])
+    maxIndexPercent[0] = 100 * maxIndex[0]/count
+    tree.Fill()
+
+rootFile.Write()
